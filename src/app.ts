ebb5f59c2c97f8be6feb7a472a54a31d78593ca7
diabyml <diaby223.ml@gmail.com>
2023-01-28 21:41:59 +0000

    graphql setup

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.use("/", (_, res) => res.send(`<h1>Hello, world from Express</h1>`));
+// app.use("/", (_, res) => res.send(`<h1>Hello, world from Express</h1>`));
 
 app.use("/test", (_, res) => res.send("<h2>Testing express app</h2>"));
 
